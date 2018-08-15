4f382305b5d56ff82beb94dcb3e04e895b70ae0d
yurakhodanitcky <yurakhodanitcky@gmail.com>
2018-08-15 12:43:15 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+﻿import { Component } from '@angular/core';
+import {OnInit} from '@angular/core';
+import {AuthenticationService} from "./_services";
+
+@Component({
+    selector: 'app',
+    templateUrl: 'app.component.html'
+})
+
+export class AppComponent implements OnInit{
+    ngOnInit() {
+    }
+
+    constructor (private authenticationService: AuthenticationService){}
+
+    isAuthenticated() {
+        return this.authenticationService.isAuthenticated();
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+﻿const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.ts',
+
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: ['ts-loader', 'angular2-template-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(html|css)$/,
+        loader: 'raw-loader'
+      },
+    ]
+  },
+  
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ],
+
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+    runtimeChunk: true
+  },
+
+  devServer: {
+    historyApiFallback: true,
+	proxy: [/*{
+            context: [
+                '/',
+     			'/api',
+                '/management',
+                '/swagger-ui.html',
+                '/v2/api-docs',
+                '/h2-console',
+                '/auth'
+            ],
+            target: 'http://127.0.0.1:8080',
+            secure: false
+        },*/{
+            context: [
+                '/websocket'
+            ],
+            target: 'ws://127.0.0.1:8080',
+            ws: true
+        }]
+  }
+};
